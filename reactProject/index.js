85acc76e6a4046cd97d850821f17bb68e5bb0edd
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 12:46:35 +0100

    Practica 4 punto 19 Listing-4.15-ProfileCard-Step4-TextComponents

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-4.14-ProfileCard-Step3-Layout/App';
+import App from './Listing-4.15-ProfileCard-Step4-TextComponents/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
