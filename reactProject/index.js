f45962b3a7cddd289d20fb34ed6efd5eabeda0ce
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 12:47:34 +0100

    Practica 4 punto 20 Listing-4.16-MonospacedFonts

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-4.15-ProfileCard-Step4-TextComponents/App';
+import App from './Listing-4.16-MonospacedFonts/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
