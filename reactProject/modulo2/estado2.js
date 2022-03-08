b518a4e7d77b599d0569aeeccdd589663c414b2c
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 10:05:38 +0100

    Practica 3 ejercicio 2 B

--- reactProject/modulo2/estado2.js
+++ reactProject/modulo2/estado2.js
@@ -6,17 +6,19 @@ class MyComponent extends React.Component {
     constructor() {
         super()
         this.state = {
-            year: 2016,
-            name: 'Nader Dabit',
-            colors: ['blue']
+            year: 2016
         }
+    }
+    updateYear() {
+        this.setState({
+            year: 2017
+        })
     }
     render() {
         return (
             <View>
-                <Text>My name is: {this.state.name}</Text>
-                <Text>The year is: {this.state.year}</Text>
-                <Text>My colors are {this.state.colors[0]}</Text>
+                <Text
+                    onPress={() => this.updateYear()}>The year is: {this.state.year}</Text>
             </View>
         )
     }
