3fee42adc3d5f928af99840a2772dba42b488ef7
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 12:38:26 +0100

    Practica 4 punto 14 Listing-4.10-ProfileCard-Step2-IncorporateBorder

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-4.9-BorderRadiusCombinations/App';
+import App from './Listing-4.10-ProfileCard-Step2-IncorporateBorder/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
