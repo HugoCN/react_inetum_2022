61c6d7882c413e0e643252b50417d01ffe48ee14
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 12:52:01 +0100

    Practica 4 punto 24 Listing-4.20-ProfileCard-Step6-Completed

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-4.19-TextStyling/App';
+import App from './Listing-4.20-ProfileCard-Step6-Completed/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
