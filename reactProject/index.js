88cc3f16203777f36aeb1086018594e5ffa9f0e8
HugoCN <hugo.cortijo@gmail.com>
2022-03-08 12:40:18 +0100

    Practica 4 punto 15 Listing-4.11-Margin

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-4.10-ProfileCard-Step2-IncorporateBorder/App';
+import App from './Listing-4.11-Margin/App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
