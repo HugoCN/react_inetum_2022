fc5563e39bad3df34efec514a4cdba63619f0def
GFI-ES\hugo.cortijo <hugo.cortijo@inetum.world>
2022-03-10 09:39:59 +0100

    practica7 punto 3

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-5.10-Flexwrap/App';
+import App from './Listing-7.1-UsingAnimatedandUpdatingtheMarginTopProperty/AnimatedTiming';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
