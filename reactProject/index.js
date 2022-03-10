08ceb1ca6e3225c658bf0bf77113d83e9fda8c1b
GFI-ES\hugo.cortijo <hugo.cortijo@inetum.world>
2022-03-10 12:17:03 +0100

    Practica 7 punto 8

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-7.5-Creating-a-sequence-of-animations/AnimatedSequence';
+import App from './Listing-7.6-Using-AnimatedStagger-to-stagger-a-series-of-animations/AnimatedStagger';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
