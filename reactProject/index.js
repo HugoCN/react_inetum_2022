78eeb696e0540e047369fa5fdf259b3a4576eb2e
GFI-ES\hugo.cortijo <hugo.cortijo@inetum.world>
2022-03-10 10:37:47 +0100

    Practica7 punto 4

--- reactProject/index.js
+++ reactProject/index.js
@@ -1,7 +1,7 @@
 import { registerRootComponent } from 'expo';
 
 //import App from './App';
-import App from './Listing-7.1-UsingAnimatedandUpdatingtheMarginTopProperty/AnimatedTiming';
+import App from './Listing-7.2-Animating-TextInput-Expand-Input-Focused/AnimatedInput';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
